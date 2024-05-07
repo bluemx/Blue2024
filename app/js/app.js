00f05c09d0a8e7799e5eb1e742344968e0e43193
Albin Rodriguez <ealbinu@gmail.com>
2024-05-07 13:52:58 -0600

    updates

--- app/js/app.js
+++ app/js/app.js
@@ -1,36 +1,38 @@
 
 
 
-
+/*********************************************************************** */
 const navbarFN = () => {
-
-
-    window.addEventListener('scroll', ()=> {
-
-            const navbar = document.querySelector('.navbar');
-            if (window.scrollY > 100) {
-                navbar.classList.remove('navbar-dark');
-                navbar.classList.add('navbar-light');
-            } else {
-                navbar.classList.remove('navbar-light');
-                navbar.classList.add('navbar-dark');
-            }
-
-    });
+    const navbar = document.querySelector('.navbar');
+    if(window.location.pathname=='/' || window.location.pathname=='/index.html'){
+        window.addEventListener('scroll', ()=> {
+                if (window.scrollY > 100) {
+                    navbar.classList.remove('navbar-dark');
+                    navbar.classList.add('navbar-light');
+                } else {
+                    navbar.classList.remove('navbar-light');
+                    navbar.classList.add('navbar-dark');
+                }
+            });
+    } else {
+        document.body.classList.add('internal-section')
+        navbar.classList.remove('navbar-dark');
+        navbar.classList.add('navbar-light');
+    }
 }
-
+/*********************************************************************** */
 const languageFN = () => {
     setTimeout(()=>{
         MicroLang(['es', 'en'])
     }, 200)
 }
-
+/*********************************************************************** */
 const cookiesFN = () => {
     const information = '<div data-l="This website uses cookies to improve your experience. Continue to use the site as normal if you agree to the use of cookies. If you’d like to find out more about the cookies we use or to opt-out, please see our Privacy Policy"> Este sitio web utiliza cookies para mejorar tu experiencia. Si continúas navegando en el sitio, significa que aceptas el uso de las cookies. Si deseas más información sobre las cookies que empleamos y cómo manejarlas visita nuestra Política de Privacidad. <a href="#" style="color: #061E41" data-toggle="modal" data-target="#aviso" data-l="Privacy Policy.">Leer más.</a></div>'
     const button = '<span data-l="Accept">Aceptar</span>'
     BlueCookies(information, "✔ Cookies", button, 'background-color: #8bb8e8; color:#fff;')
 }
-
+/*********************************************************************** */
 const bannerFN = () => {
     if(!document.querySelector('.bluebanner')){ return false }
     var banner = new Glide('.glide',{
