4aec01fbae8f3ab07d537404742e5e99d6c95fa7
Albin Rodriguez <ealbinu@gmail.com>
2024-06-27 12:52:01 -0600

    update form

--- app/js/form.js
+++ app/js/form.js
@@ -1,240 +1,165 @@
 setTimeout(() => {
-  var _0x77e5 = ['use strict', 'load', 'val', '#HfLanguage', 'preventDefault', 'hide', '#PopupServices', 'click', '.ClosePopupServices', 'show', '#DdlService', 'length', "input[name='ChkBxLstServices']:checked", 'es', 'Seleccione', 'html', 'Select', 'was-validated', 'hasClass', '#ContactForm', '.invalid-feedback', 'next', 'is-valid-select', 'removeClass', 'is-invalid-select', 'addClass', 'data-service', 'attr', 'data-service-l', ' servicios seleccionados', ' services selected', "input[name='ChkBxLstServices']", 'needs-validation', 'getElementsByClassName', 'submit', 'disabled', 'prop', '#BtnSubmit', 'checkValidity', 'stopPropagation', ':checked', 'is', '#ChkBxTerms', 'find', 'div', 'parent', 'value', 'push', 'each', 'eyjhbgcioijiuzi1niisinr5cci6ikpxvcj9.eyjuyw1lijoiz…sesj9.3hwn-cejlo_uqabbf4j5bhbwjhgev-iwvmx-rgl0wpc', '#TbxName', '#TbxCompany', '#TbxEmail', '#TbxPhone', '#HfCountry', '#TbxComments', 'stringify', 'https://www.blueux.mx/contactapi/api/Contacts', 'application/json; charset=utf-8', 'json', 'post', 'ResultID', '', "<div class='mppm-popup-item-icon'><img src='", 'data-image', "' /></div><div class='mppm-popup-item-text'>", '</div>', "Gracias por tu interés por descubrir el poder de la lealtad con <div class='mppm-popup-item'>", '</div> Nos pondremos en contacto contigo lo antes posible', "Thank you for your interest to discover the power of loyalty with <div class='mppm-popup-item'>", '</div> We will contact you as soon as possible', '#popup_message', '#PopupMessage', 'checked', '0', 'input[name=ChkBxTerms]', 'on', '.ClosePopupMessage', 'Description', 'Hubo un error al envíar el formulario, favor de revisar su conexión a internet', 'There was an error sending the form, please check your internet connection', "<img src='https://bluepureloyalty.com/app/images/mail/001-warning.png' /><div>", 'ajax', 'add', 'classList', 'addEventListener', 'call', 'filter', 'prototype', 'ChkBxTerms', 'getElementById'];
+	var selectedLanguage = $('#HfLanguage').val();
+      
+	$('.ClosePopupServices').click(function (event) {
+	  event.preventDefault();
+	  $('#PopupServices').hide();
+	});
+	
+	$('#DdlService').click(function () {
+	  $('#PopupServices').show();
+	});
+	
+	$("input[name='ChkBxLstServices']").click(function () {
+	  if ($("input[name='ChkBxLstServices']:checked").length === 0) {
+		if ($('#HfLanguage').val() == 'es') {
+		  $('#DdlService').html('Seleccione');
+		} else {
+		  $('#DdlService').html('Select');
+		}
+		if ($('#ContactForm').hasClass('was-validated')) {
+		  $('#DdlService').removeClass('is-valid-select').addClass('is-invalid-select');
+		}
+	  } else if ($("input[name='ChkBxLstServices']:checked").length === 1) {
+		if ($('#HfLanguage').val() == 'es') {
+		  $('#DdlService').html($(this).attr('data-service'));
+		} else {
+		  $('#DdlService').html($(this).attr('data-service-l'));
+		}
+		if ($('#ContactForm').hasClass('was-validated')) {
+		  $('#DdlService').removeClass('is-invalid-select').addClass('is-valid-select');
+		}
+	  } else {
+		if ($('#HfLanguage').val() == 'es') {
+		  $('#DdlService').html($("input[name='ChkBxLstServices']:checked").length + ' servicios seleccionados');
+		} else {
+		  $('#DdlService').html($("input[name='ChkBxLstServices']:checked").length + ' services selected');
+		}
+		if ($('#ContactForm').hasClass('was-validated')) {
+		  $('#DdlService').removeClass('is-invalid-select').addClass('is-valid-select');
+		}
+	  }
+	});
 
+	var forms = document.getElementsByClassName('needs-validation');
+	var validation = Array.prototype.filter.call(forms, function (form) {
+	  form.addEventListener('submit', function (event) {
+		$('#BtnSubmit').prop('disabled', true);
+		if (form.checkValidity() === false) {
+		  $('#BtnSubmit').prop('disabled', false);
+		  event.preventDefault();
+		  event.stopPropagation();
+		  if (!$('#ChkBxTerms').is(':checked')) {
+			$('#ChkBxTerms').parent('div').find('.invalid-feedback').show();
+		  }
+		  if ($("input[name='ChkBxLstServices']:checked").length === 0) {
+			$('#DdlService').removeClass('is-valid-select').addClass('is-invalid-select');
+		  } else {
+			$('#DdlService').removeClass('is-invalid-select').addClass('is-valid-select');
+		  }
+		} else {
+		  if (!$('#ChkBxTerms').is(':checked')) {
+			$('#BtnSubmit').prop('disabled', false);
+			event.preventDefault();
+			event.stopPropagation();
+			$('#ChkBxTerms').parent('div').find('.invalid-feedback').show();
+		  } else {
+			if ($("input[name='ChkBxLstServices']:checked").length === 0) {
+			  $('#BtnSubmit').prop('disabled', false);
+			  event.preventDefault();
+			  event.stopPropagation();
+			  $('#DdlService').removeClass('is-valid-select').addClass('is-invalid-select');
+			} else {
+			  event.preventDefault();
+			  event.stopPropagation();
+			  var selectedServices = [];
+			  $("input[name='ChkBxLstServices']:checked").each(function () {
+				selectedServices.push(this.value);
+			  });
+			  var tokenID = 'eyJhbgcioijiuzi1niisinr5cci6ikpxvcj9.eyjuyw1lijoiz…sesj9.3hwn-cejlo_uqabbf4j5bhbwjhgev-iwvmx-rgl0wpc';
+			  tokenID = btoa(tokenID);
+			  var data = JSON.stringify({
+				"tokenID": tokenID,
+				"name": $('#TbxName').val(),
+				"companyName": $('#TbxCompany').val(),
+				"email": $('#TbxEmail').val(),
+				"phone": $('#TbxPhone').val(),
+				"serviceId": selectedServices,
+				"country": $('#HfCountry').val(),
+				"language": $('#HfLanguage').val(),
+				"comments": $('#TbxComments').val(),
+			  });
 
-
-  (function () {
-    _0x77e5[0];
-    window[_0x77e5[85]](
-      _0x77e5[1],
-      function () {
-        var _0xba79x1 = $(_0x77e5[3])[_0x77e5[2]]();
-        $(_0x77e5[8])[_0x77e5[7]](function (_0xba79x2) {
-          _0xba79x2[_0x77e5[4]]();
-          $(_0x77e5[6])[_0x77e5[5]]();
-        });
-        $(_0x77e5[10])[_0x77e5[7]](function () {
-          $(_0x77e5[6])[_0x77e5[9]]();
-        });
-        $(_0x77e5[31])[_0x77e5[7]](function () {
-          if ($(_0x77e5[12])[_0x77e5[11]] === 0) {
-            if ($(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]) {
-              $(_0x77e5[10])[_0x77e5[15]](_0x77e5[14]);
-            } else {
-              $(_0x77e5[10])[_0x77e5[15]](_0x77e5[16]);
-            }
-            if ($(_0x77e5[19])[_0x77e5[18]](_0x77e5[17])) {
-              $(_0x77e5[10])[_0x77e5[21]](_0x77e5[20])[_0x77e5[9]]();
-              $(_0x77e5[10])[_0x77e5[23]](_0x77e5[22]);
-              $(_0x77e5[10])[_0x77e5[25]](_0x77e5[24]);
-            }
-          } else {
-            if ($(_0x77e5[12])[_0x77e5[11]] === 1) {
-              if ($(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]) {
-                $(_0x77e5[10])[_0x77e5[15]]($(this)[_0x77e5[27]](_0x77e5[26]));
-              } else {
-                $(_0x77e5[10])[_0x77e5[15]]($(this)[_0x77e5[27]](_0x77e5[28]));
-              }
-              if ($(_0x77e5[19])[_0x77e5[18]](_0x77e5[17])) {
-                $(_0x77e5[10])[_0x77e5[21]](_0x77e5[20])[_0x77e5[5]]();
-                $(_0x77e5[10])[_0x77e5[23]](_0x77e5[24]);
-                $(_0x77e5[10])[_0x77e5[25]](_0x77e5[22]);
-              }
-            } else {
-              if ($(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]) {
-                $(_0x77e5[10])[_0x77e5[15]](
-                  $(_0x77e5[12])[_0x77e5[11]] + _0x77e5[29]
-                );
-              } else {
-                $(_0x77e5[10])[_0x77e5[15]](
-                  $(_0x77e5[12])[_0x77e5[11]] + _0x77e5[30]
-                );
-              }
-              if ($(_0x77e5[19])[_0x77e5[18]](_0x77e5[17])) {
-                $(_0x77e5[10])[_0x77e5[21]](_0x77e5[20])[_0x77e5[5]]();
-                $(_0x77e5[10])[_0x77e5[23]](_0x77e5[24]);
-                $(_0x77e5[10])[_0x77e5[25]](_0x77e5[22]);
-              }
-            }
-          }
-        });
-        var _0xba79x3 = document[_0x77e5[33]](_0x77e5[32]);
-        var _0xba79x4 = Array[_0x77e5[88]][_0x77e5[87]][_0x77e5[86]](
-          _0xba79x3,
-          function (_0xba79x5) {
-            _0xba79x5[_0x77e5[85]](
-              _0x77e5[34],
-              function (_0xba79x6) {
-                $(_0x77e5[37])[_0x77e5[36]](_0x77e5[35], true);
-                if (_0xba79x5[_0x77e5[38]]() === false) {
-                  $(_0x77e5[37])[_0x77e5[36]](_0x77e5[35], false);
-                  _0xba79x6[_0x77e5[4]]();
-                  _0xba79x6[_0x77e5[39]]();
-                  if (!$(_0x77e5[42])[_0x77e5[41]](_0x77e5[40])) {
-                    $(_0x77e5[42])
-                      [_0x77e5[45]](_0x77e5[44])
-                      [_0x77e5[43]](_0x77e5[20])
-                      [_0x77e5[9]]();
-                  }
-                  if ($(_0x77e5[12])[_0x77e5[11]] === 0) {
-                    $(_0x77e5[10])[_0x77e5[21]](_0x77e5[20])[_0x77e5[9]]();
-                    $(_0x77e5[10])[_0x77e5[23]](_0x77e5[22]);
-                    $(_0x77e5[10])[_0x77e5[25]](_0x77e5[24]);
-                  } else {
-                    $(_0x77e5[10])[_0x77e5[21]](_0x77e5[20])[_0x77e5[5]]();
-                    $(_0x77e5[10])[_0x77e5[23]](_0x77e5[24]);
-                    $(_0x77e5[10])[_0x77e5[25]](_0x77e5[22]);
-                  }
-                } else {
-                  if (!$(_0x77e5[42])[_0x77e5[41]](_0x77e5[40])) {
-                    $(_0x77e5[37])[_0x77e5[36]](_0x77e5[35], false);
-                    _0xba79x6[_0x77e5[4]]();
-                    _0xba79x6[_0x77e5[39]]();
-                    $(_0x77e5[42])
-                      [_0x77e5[45]](_0x77e5[44])
-                      [_0x77e5[43]](_0x77e5[20])
-                      [_0x77e5[9]]();
-                  } else {
-                    if ($(_0x77e5[12])[_0x77e5[11]] === 0) {
-                      $(_0x77e5[37])[_0x77e5[36]](_0x77e5[35], false);
-                      _0xba79x6[_0x77e5[4]]();
-                      _0xba79x6[_0x77e5[39]]();
-                      $(_0x77e5[10])[_0x77e5[21]](_0x77e5[20])[_0x77e5[9]]();
-                      $(_0x77e5[10])[_0x77e5[23]](_0x77e5[22]);
-                      $(_0x77e5[10])[_0x77e5[25]](_0x77e5[24]);
-                    } else {
-                      _0xba79x6[_0x77e5[4]]();
-                      _0xba79x6[_0x77e5[39]]();
-                      var _0xba79x7 = [];
-                      $(_0x77e5[12])[_0x77e5[48]](function () {
-                        _0xba79x7[_0x77e5[47]](this[_0x77e5[46]]);
-                      });
-                      var _0xba79x8 = _0x77e5[49];
-                      _0xba79x8 = btoa(_0xba79x8);
-                      var _0xba79x9 = JSON[_0x77e5[56]]({
-                        "\x74\x6F\x6B\x65\x6E\x49\x44": _0xba79x8,
-                        "\x6E\x61\x6D\x65": $(_0x77e5[50])[_0x77e5[2]](),
-                        "\x63\x6F\x6D\x70\x61\x6E\x79\x4E\x61\x6D\x65": $(
-                          _0x77e5[51]
-                        )[_0x77e5[2]](),
-                        "\x65\x6D\x61\x69\x6C": $(_0x77e5[52])[_0x77e5[2]](),
-                        "\x70\x68\x6F\x6E\x65": $(_0x77e5[53])[_0x77e5[2]](),
-                        "\x73\x65\x72\x76\x69\x63\x65\x49\x64": _0xba79x7,
-                        "\x63\x6F\x75\x6E\x74\x72\x79": $(_0x77e5[54])[
-                          _0x77e5[2]
-                        ](),
-                        "\x6C\x61\x6E\x67\x75\x61\x67\x65": $(_0x77e5[3])[
-                          _0x77e5[2]
-                        ](),
-                        "\x63\x6F\x6D\x6D\x65\x6E\x74\x73": $(_0x77e5[55])[
-                          _0x77e5[2]
-                        ](),
-                      });
-                      $[_0x77e5[82]]({
-                        url: _0x77e5[57],
-                        data: _0xba79x9,
-                        contentType: _0x77e5[58],
-                        datatype: _0x77e5[59],
-                        type: _0x77e5[60],
-                        success: function (_0xba79xa) {
-                          if (_0xba79xa[_0x77e5[61]] > 0) {
-                            var _0xba79xb = _0x77e5[62];
-                            var _0xba79xc = _0x77e5[62];
-                            var _0xba79xd = _0x77e5[62];
-                            $(_0x77e5[12])[_0x77e5[48]](function () {
-                              if ($(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]) {
-                                _0xba79xc =
-                                  _0xba79xc +
-                                  _0x77e5[63] +
-                                  $(this)[_0x77e5[27]](_0x77e5[64]) +
-                                  _0x77e5[65] +
-                                  $(this)[_0x77e5[27]](_0x77e5[26]) +
-                                  _0x77e5[66];
-                              } else {
-                                _0xba79xc =
-                                  _0xba79xc +
-                                  _0x77e5[63] +
-                                  $(this)[_0x77e5[27]](_0x77e5[64]) +
-                                  _0x77e5[65] +
-                                  $(this)[_0x77e5[27]](_0x77e5[28]) +
-                                  _0x77e5[66];
-                              }
-                            });
-                            if ($(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]) {
-                              _0xba79xb = _0x77e5[67] + _0xba79xc + _0x77e5[68];
-                            } else {
-                              _0xba79xb = _0x77e5[69] + _0xba79xc + _0x77e5[70];
-                            }
-                            $(_0x77e5[71])[_0x77e5[15]](_0xba79xb);
-                            $(_0x77e5[72])[_0x77e5[9]]();
-                            $(_0x77e5[77])[_0x77e5[76]](
-                              _0x77e5[7],
-                              function (_0xba79x2) {
-                                $(_0x77e5[50])[_0x77e5[2]](_0x77e5[62]);
-                                $(_0x77e5[51])[_0x77e5[2]](_0x77e5[62]);
-                                $(_0x77e5[52])[_0x77e5[2]](_0x77e5[62]);
-                                $(_0x77e5[53])[_0x77e5[2]](_0x77e5[62]);
-                                if (
-                                  $(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]
-                                ) {
-                                  $(_0x77e5[10])[_0x77e5[15]](_0x77e5[14]);
-                                } else {
-                                  $(_0x77e5[10])[_0x77e5[15]](_0x77e5[16]);
-                                }
-                                $(_0x77e5[31])[_0x77e5[48]](function () {
-                                  $(this)[_0x77e5[36]](_0x77e5[73], false);
-                                });
-                                $(_0x77e5[37])[_0x77e5[36]](_0x77e5[35], false);
-                                $(_0x77e5[75])[_0x77e5[2]](_0x77e5[74]);
-                                $(_0x77e5[75])[_0x77e5[36]](_0x77e5[73], false);
-                                $(_0x77e5[55])[_0x77e5[2]](_0x77e5[62]);
-                                $(_0x77e5[19])[_0x77e5[23]](_0x77e5[17]);
-                                $(_0x77e5[10])
-                                  [_0x77e5[21]](_0x77e5[20])
-                                  [_0x77e5[5]]();
-                                $(_0x77e5[10])[_0x77e5[23]](_0x77e5[22]);
-                                $(_0x77e5[10])[_0x77e5[23]](_0x77e5[24]);
-                                $(_0x77e5[72])[_0x77e5[5]]();
-                              }
-                            );
-                          } else {
-                            var _0xba79xb = _0x77e5[62];
-                            try {
-                              _0xba79xb = _0xba79xa[_0x77e5[78]];
-                            } catch (e) {
-                              if ($(_0x77e5[3])[_0x77e5[2]]() == _0x77e5[13]) {
-                                _0xba79xb = _0x77e5[79];
-                              } else {
-                                _0xba79xb = _0x77e5[80];
-                              }
-                            }
-                            $(_0x77e5[71])[_0x77e5[15]](
-                              _0x77e5[81] + _0xba79xb + _0x77e5[66]
-                            );
-                            $(_0x77e5[72])[_0x77e5[9]]();
-                            $(_0x77e5[37])[_0x77e5[36]](_0x77e5[35], false);
-                          }
-                        },
-                      });
-                    }
-                  }
-                }
-                _0xba79x5[_0x77e5[84]][_0x77e5[83]](_0x77e5[17]);
-              },
-              false
-            );
-          }
-        );
-        var _0xba79xe = document[_0x77e5[90]](_0x77e5[89]);
-        _0xba79xe[_0x77e5[85]](_0x77e5[7], function () {
-          $(_0x77e5[42])
-            [_0x77e5[45]](_0x77e5[44])
-            [_0x77e5[43]](_0x77e5[20])
-            [_0x77e5[5]]();
-        });
-      },
-      false
-    );
-  })();
-  console.log("form loaded");
+			  $.ajax({
+				url: 'https://www.blueux.mx/contactapi/api/Contacts',
+				data: data,
+				contentType: 'application/json; charset=utf-8',
+				datatype: 'json',
+				type: 'post',
+				success: function (result) {
+				  if (result.ResultID > 0) {
+					var message = '';
+					var serviceList = '';
+					$("input[name='ChkBxLstServices']:checked").each(function () {
+					  if ($('#HfLanguage').val() == 'es') {
+						serviceList += "<div class='mppm-popup-item-icon'><img src='" + $(this).attr('data-image') + "' /></div><div class='mppm-popup-item-text'>" + $(this).attr('data-service') + "</div>";
+					  } else {
+						serviceList += "<div class='mppm-popup-item-icon'><img src='" + $(this).attr('data-image') + "' /></div><div class='mppm-popup-item-text'>" + $(this).attr('data-service-l') + "</div>";
+					  }
+					});
+					if ($('#HfLanguage').val() == 'es') {
+					  message = "Gracias por tu interés por descubrir el poder de la lealtad con <div class='mppm-popup-item'>" + serviceList + "</div> Nos pondremos en contacto contigo lo antes posible";
+					} else {
+					  message = "Thank you for your interest to discover the power of loyalty with <div class='mppm-popup-item'>" + serviceList + "</div> We will contact you as soon as possible";
+					}
+					$('#popup_message').html(message);
+					$('#PopupMessage').show();
+					$('.ClosePopupMessage').on('click', function (event) {
+					  $('#TbxName').val('');
+					  $('#TbxCompany').val('');
+					  $('#TbxEmail').val('');
+					  $('#TbxPhone').val('');
+					  if ($('#HfLanguage').val() == 'es') {
+						$('#DdlService').html('Seleccione');
+					  } else {
+						$('#DdlService').html('Select');
+					  }
+					  $("input[name='ChkBxLstServices']").each(function () {
+						$(this).prop('checked', false);
+					  });
+					  $('#BtnSubmit').prop('disabled', false);
+					  $('#BtnSubmit').removeClass('was-validated');
+					  $('#TbxComments').val('');
+					  $('#ContactForm').removeClass('was-validated');
+					  $('#PopupMessage').hide();
+					});
+				  } else {
+					var errorMessage = '';
+					try {
+					  errorMessage = result.Description;
+					} catch (e) {
+					  if ($('#HfLanguage').val() == 'es') {
+						errorMessage = 'Hubo un error al envíar el formulario, favor de revisar su conexión a internet';
+					  } else {
+						errorMessage = 'There was an error sending the form, please check your internet connection';
+					  }
+					}
+					$('#popup_message').html("<img src='https://bluepureloyalty.com/app/images/mail/001-warning.png' /><div>" + errorMessage + "</div>");
+					$('#PopupMessage').show();
+					$('#BtnSubmit').prop('disabled', false);
+				  }
+				},
+			  });
+			}
+		  }
+		}
+		form.classList.add('was-validated');
+	  }, false);
+	});
+	
+	var termsCheckbox = document.getElementById('ChkBxTerms');
+	termsCheckbox.addEventListener('click', function () {
+	  $('#ChkBxTerms').parent('div').find('.invalid-feedback').hide();
+	});
+  console.log("form loaded v-4");
 }, 1000);
